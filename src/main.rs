648fac168e0ddf3493fb80ce6c1a12b22539a4de
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-02-03 13:38:09 +0100

    main.rs: Clean up the code

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,8 @@ extern crate agp_lib;
 use agp_lib::audiograph_parser::parse_audiograph_from_file;
 use agp_lib::puredata_parser::parse_puredata_from_file;
 
-use std::fs::File;
-use std::io::*;
-
 fn main() {
-    let mut pd_parse = parse_puredata_from_file("./Samples/PD/aleatoire.pd");
+    parse_puredata_from_file("./Samples/PD/aleatoire.pd");
 
-    let mut ag_parse = parse_audiograph_from_file("./Samples/AG/test.ag");
+    parse_audiograph_from_file("./Samples/AG/test.ag");
 }
