17d8aa797dd72a3da262f02116cb80bdea89c310
Ivan <delgado.ivan@protonmail.com>
2019-02-18 21:53:15 +0100

    implementation HLFET :     *fonction privée get_min sur la HashMap<usize,f64> , possibilité d'optimisation en passant par une structure HeapMax   *algorithme d'ordonnancement HLET (TaskGraph * uzise ) -> Schedule

---
+++ src/scheduling/static_alg.rs
@@ -0,0 +1,73 @@
+use std::collections::HashMap;
+
+use task_graph::{graph::TaskGraph, node::Node, state::TaskState};
+
+use scheduling::{processor::Processor, schedule::Schedule, timeslot::TimeSlot};
+
+//return the minimum value from a ready list
+//ties broken by number of successors (most first)
+fn get_max_tie_misf(ready_list: &HashMap<usize, f64>, ref graph: &TaskGraph) -> usize {
+    let mut out_node: Option<usize> = None;
+
+    for (node, b_level) in ready_list {
+        if out_node == None {
+            out_node = Some(*node);
+        } else {
+            if *b_level == *ready_list.get(&out_node.unwrap()).unwrap() {
+                if graph.get_successors(*node) > graph.get_successors(out_node.unwrap()) {
+                    out_node = Some(*node);
+                }
+            } else {
+                if *b_level > *ready_list.get(&out_node.unwrap()).unwrap() {
+                    out_node = Some(*node);
+                }
+            }
+        }
+    }
+
+    out_node.unwrap()
+}
+
+pub fn hlfet(graph: &mut TaskGraph, nb_processors: usize) -> Schedule {
+    let mut out_schedule = Schedule::new();
+
+    for _ in 0..nb_processors {
+        out_schedule.add_processor();
+    }
+
+    //Nb : graph is of type <& mut TaskGraph> because
+    //get_entry_node is on <& mut self>
+    let first_nodes = graph.get_entry_nodes();
+
+    let mut ready_list: HashMap<usize, f64> = HashMap::new();
+
+    for node in first_nodes {
+        ready_list.insert(node, graph.get_b_level(node).unwrap());
+    }
+
+    while !ready_list.is_empty() {
+        
+        let first_node = get_max_tie_misf(&mut ready_list, graph);
+        ready_list.remove(&first_node);
+
+        let mut first_proc = 0;
+        let mut first_start_time = out_schedule.processors[first_proc].get_completion_time();
+
+        for i in 1..out_schedule.processors.len() {
+            let current_start_time = out_schedule.processors[i].get_completion_time();
+            if current_start_time < first_start_time {
+                first_proc = i;
+                first_start_time = current_start_time;
+            }
+        }
+
+        out_schedule.processors[first_proc].add_timeslot(
+            first_node,
+            first_start_time,
+            first_start_time + graph.get_wcet(first_node).unwrap(),
+        );
+
+    }
+
+    out_schedule
+}
