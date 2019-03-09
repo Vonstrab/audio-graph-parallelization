d1659da0c3c5a742cf2c1aef47fbb2614f0ba22e
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-03-09 01:09:21 +0100

    parser.rs: Avoid using nested function declarations

--- src/parser/audiograph/parser.rs
+++ src/parser/audiograph/parser.rs
@@ -25,89 +25,88 @@ pub struct Edge {
 #[grammar = "parser/audiograph/audiograph.pest"]
 pub struct AudiographParser;
 
+fn parse_node(pair: Pair<Rule>) -> Task {
+    let mut inner_rules = pair.into_inner();
+    let id: String;
+    let mut nb_inlets: u32 = 0;
+    let mut nb_outlets: u32 = 0;
+    let mut class_name: String = String::default();
+    let mut text: Option<String> = None;
+    let mut wcet: Option<f64> = None;
+    let mut more: HashMap<String, String> = HashMap::new();
+    let mut volume: f32 = 0.0;
+
+    id = inner_rules.next().unwrap().as_str().to_string();
+
+    //Attributes
+    for attribute in inner_rules {
+        let mut attr = attribute.into_inner();
+        let token = attr.next().unwrap().as_str().trim_matches('\"');
+        let v = attr.next().unwrap().as_str().trim_matches('\"');
+        match token {
+            "in" => nb_inlets = v.parse().unwrap(),
+            "out" => nb_outlets = v.parse().unwrap(),
+            "text" => text = Some(v.to_string()),
+            "kind" => class_name = v.to_string(),
+            "wcet" => wcet = Some(v.parse().unwrap()),
+            "volume" => volume = v.parse().unwrap(),
+            _ => {
+                more.insert(token.to_string(), v.to_string());
+            }
+        }
+    }
+
+    Task::Audiograph {
+        id,
+        nb_inlets,
+        nb_outlets,
+        class_name,
+        text,
+        wcet,
+        more,
+        volume,
+    }
+}
+
+fn parse_edge(pair: Pair<Rule>) -> IntoIter<Edge> {
+    let mut inner_rules = pair.into_inner();
+    let mut port_ident = inner_rules.next().unwrap().into_inner();
+    let mut src_id = port_ident.next().unwrap().as_str().to_string();
+    let mut src_port = port_ident.next().unwrap().as_str().parse().unwrap();
+
+    let mut edges = Vec::new();
+
+    for inner_rule in inner_rules {
+        port_ident = inner_rule.into_inner().next().unwrap().into_inner();
+        let dst_id = port_ident.next().unwrap().as_str().to_string();
+        let dst_port = port_ident.next().unwrap().as_str().parse().unwrap();
+
+        edges.push(Edge {
+            src_id,
+            src_port,
+            dst_id: dst_id.clone(),
+            dst_port,
+        });
+
+        src_id = dst_id;
+        src_port = dst_port;
+    }
+
+    edges.into_iter()
+}
+
 pub fn parse_audiograph(audiograph: &str) -> Result<graph::TaskGraph, ParseError<Rule>> {
     let audiograph = AudiographParser::parse(Rule::file, audiograph)?
         .next()
         .unwrap();
 
-    use pest::iterators::*;
-
-    fn parse_node(pair: Pair<Rule>) -> Task {
-        let mut inner_rules = pair.into_inner();
-        let id: String;
-        let mut nb_inlets: u32 = 0;
-        let mut nb_outlets: u32 = 0;
-        let mut class_name: String = String::default();
-        let mut text: Option<String> = None;
-        let mut wcet: Option<f64> = None;
-        let mut more: HashMap<String, String> = HashMap::new();
-        let mut volume: f32 = 0.0;
-
-        id = inner_rules.next().unwrap().as_str().to_string();
-
-        //Attributes
-        for attribute in inner_rules {
-            let mut attr = attribute.into_inner();
-            let token = attr.next().unwrap().as_str().trim_matches('\"');
-            let v = attr.next().unwrap().as_str().trim_matches('\"');
-            match token {
-                "in" => nb_inlets = v.parse().unwrap(),
-                "out" => nb_outlets = v.parse().unwrap(),
-                "text" => text = Some(v.to_string()),
-                "kind" => class_name = v.to_string(),
-                "wcet" => wcet = Some(v.parse().unwrap()),
-                "volume" => volume = v.parse().unwrap(),
-                _ => {
-                    more.insert(token.to_string(), v.to_string());
-                }
-            }
-        }
-
-        Task::Audiograph {
-            id,
-            nb_inlets,
-            nb_outlets,
-            class_name,
-            text,
-            wcet,
-            more,
-            volume,
-        }
-    }
-
-    use std::vec::IntoIter;
-
-    fn parse_edge(pair: Pair<Rule>) -> IntoIter<Edge> {
-        let mut inner_rules = pair.into_inner();
-        let mut port_ident = inner_rules.next().unwrap().into_inner();
-        let mut src_id = port_ident.next().unwrap().as_str().to_string();
-        let mut src_port = port_ident.next().unwrap().as_str().parse().unwrap();
-
-        let mut edges = Vec::new();
-
-        for inner_rule in inner_rules {
-            port_ident = inner_rule.into_inner().next().unwrap().into_inner();
-            let dst_id = port_ident.next().unwrap().as_str().to_string();
-            let dst_port = port_ident.next().unwrap().as_str().parse().unwrap();
-            edges.push(Edge {
-                src_id,
-                src_port,
-                dst_id: dst_id.clone(),
-                dst_port,
-            });
-            src_id = dst_id;
-            src_port = dst_port;
-        }
-        edges.into_iter()
-    }
-
-    //let to_print = audiograph.clone().into_inner().flat_map()
+    // let to_print = audiograph.clone().into_inner().flat_map()
 
     let (nodes, edges): (Vec<_>, Vec<_>) = audiograph
         .into_inner()
         .flat_map(|r| r.into_inner())
         .filter(|ref r| r.as_rule() != Rule::deadline)
-        //.inspect(|x| println!("Statement: {:?}.", x))
+        // .inspect(|x| println!("Statement: {:?}.", x))
         .partition(|ref r| r.as_rule() == Rule::node);
 
     let nodes = nodes.into_iter().map(parse_node).collect::<Vec<_>>();
@@ -146,5 +145,6 @@ pub fn parse(filename: &str) -> Result<graph::TaskGraph, ParseError<Rule>> {
 
     file.read_to_string(&mut s)
         .expect("Impossible to read file.");
+
     parse_audiograph(&s)
 }
