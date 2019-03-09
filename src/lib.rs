68beec965462d4952b7da07c221d6e59fe939cda
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-03-09 01:00:31 +0100

    lib.rs: Place module declarations after extern crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![crate_type = "lib"]
 
-pub mod parser;
-pub mod scheduling;
-pub mod task_graph;
-
 extern crate pest;
 #[macro_use]
 extern crate pest_derive;
+
+pub mod parser;
+pub mod scheduling;
+pub mod task_graph;
