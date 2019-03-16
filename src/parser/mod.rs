8e94d8f2dc8a24635a39c7acb665a7f600706f5a
Ivan <delgado.ivan@protonmail.com>
2019-03-16 16:16:39 +0100

    ajout de test dans le parser

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -12,3 +12,24 @@ pub fn parse(filename: &str) -> Option<graph::TaskGraph> {
         None
     }
 }
+
+#[cfg(test)]
+mod parser_test {
+    use super::*;
+
+    #[test]
+    fn parse_audiograph() {
+        let graph_1 = parse("Samples/AG/downsampling_test.ag").unwrap();
+        assert_eq!(graph_1.get_topological_order().len(), 5);
+        let graph_2 = parse("Samples/AG/audiograph_wcet_test.ag").unwrap();
+        assert_eq!(graph_2.get_topological_order().len(), 3);
+    }
+
+    #[test]
+    fn parse_puredata() {
+        let graph_1 = parse("Samples/PD/aleatoire.pd").unwrap();
+        assert_eq!(graph_1.get_topological_order().len(), 9);
+        let graph_2 = parse("Samples/PD/Tonalite.pd").unwrap();
+        assert_eq!(graph_2.get_topological_order().len(), 12);
+    }
+}
