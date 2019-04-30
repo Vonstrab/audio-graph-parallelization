3052fc66fcb20b7eb8675e7c8818b711bfa0cd48
Eisha Chen-yen-su <chenyensu0@gmail.com>
2019-04-30 10:46:02 +0200

    static_sched_test: Allow to choose the algorithm

--- src/static_sched_test.rs
+++ src/static_sched_test.rs
@@ -6,16 +6,27 @@ use std::sync::{Arc, RwLock};
 use crossbeam::channel::unbounded;
 
 use agp_lib::measure::Measure;
+use agp_lib::scheduling::static_alg::SchedulingAlgorithm;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
 
-    if args.len() == 1 {
-        panic!("No files supplied");
+    if args.len() != 3 {
+        panic!("Usage: static_sched_test <AG File> <{rand, hlfet, etf}>");
     }
 
     let dag = agp_lib::parser::audiograph::parser::actual_parse(&args[1])
         .expect("Failed to parse audio graph");
+
+    let sched_algo = if args[2] == "rand" {
+        SchedulingAlgorithm::Random
+    } else if args[2] == "hlfet" {
+        SchedulingAlgorithm::HLFET
+    } else if args[2] == "etf" {
+        SchedulingAlgorithm::ETF
+    } else {
+        panic!("There is no such scheduling algorithm");
+    };
 
     let (tx, rx) = unbounded();
     let mut measure_thread = Measure::new(rx);
@@ -24,7 +35,11 @@ fn main() {
         measure_thread.receive();
     });
 
-    match agp_lib::scheduling::execution::run_static_sched(Arc::new(RwLock::new(dag)), tx) {
+    match agp_lib::scheduling::execution::run_static_sched(
+        Arc::new(RwLock::new(dag)),
+        sched_algo,
+        tx,
+    ) {
         Ok(_) => {}
         e => {
             eprintln!("Failed to run because: {:?}", e);
